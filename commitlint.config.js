10d05f6b0a176f85694cb7ab94c636ea0b0cceaf
Indra Zulfi <me@indrazm.com>
2023-08-11 19:23:15 +0700

    features: project

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,34 +5,4 @@ module.exports = {
     "subject-full-stop": [0, "never"],
     "subject-case": [0, "never"],
   },
-  prompt: {
-    questions: {
-      type: {
-        description: "Select the type of change that you're committing:",
-        enum: {
-          feat: {
-            description: "A new feature",
-            title: "Features",
-            emoji: "✨",
-          },
-          fix: {
-            description: "A bug fix",
-            title: "Bug Fixes",
-            emoji: "🐛",
-          },
-          refactor: {
-            description: "A code change that neither fixes a bug nor adds a feature",
-            title: "Code Refactoring",
-            emoji: "♻️",
-          },
-        },
-      },
-      subject: {
-        description: "Write a short, imperative tense description of the change (max 50 chars):\n",
-      },
-      body: {
-        description: "Provide a longer description of the change: (press enter to skip)\n",
-      },
-    },
-  },
 };
